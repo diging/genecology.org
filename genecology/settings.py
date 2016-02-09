d3584c55caf05900391ce85b31b3ae0ad398e7d1
Erick Peirson <erick.peirson@asu.edu>
2016-02-08 19:36:44 -0500

    static config

--- genecology/settings.py
+++ genecology/settings.py
@@ -122,11 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+
 PWD = os.path.dirname(os.path.realpath(__file__))  # project root path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PWD, '..', 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MARKUP_FIELD_TYPES = (
