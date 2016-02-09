fcee4f5d3b3384cf07af899b586080a571a026b6
Erick Peirson <erick.peirson@asu.edu>
2016-02-08 19:34:53 -0500

    static config

--- genecology/heroku_settings.py
+++ genecology/heroku_settings.py
@@ -119,19 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 PWD = os.path.dirname(os.path.realpath(__file__))  # project root path
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-
 MARKUP_FIELD_TYPES = (
     ('markdown', markdown.markdown),
 )
