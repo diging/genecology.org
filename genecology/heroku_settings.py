945a6c5d67daf5dcf40fdd956e1441633aa38625
Erick Peirson <erick.peirson@asu.edu>
2016-03-16 16:37:43 -0400

    heroku config for elasticsearch

--- genecology/heroku_settings.py
+++ genecology/heroku_settings.py
@@ -143,13 +143,24 @@ MARKUP_FIELD_TYPES = (
 
 AUTH_USER_MODEL = 'blog.GenecologyUser'
 
+from urlparse import urlparse
+
+es = urlparse(os.environ.get('SEARCHBOX_URL') or 'http://127.0.0.1:9200/')
+
+port = es.port or 80
+
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': os.environ.get('SEARCHBOX_URL'),
-        'INDEX_NAME': 'haystack',
+        'URL': es.scheme + '://' + es.hostname + ':' + str(port),
+        'INDEX_NAME': 'documents',
     },
 }
+
+if es.username:
+    HAYSTACK_CONNECTIONS['default']['KWARGS'] = {"http_auth": es.username + ':' + es.password}
+
+
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
