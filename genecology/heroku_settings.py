28f84dcea5246e8eecc98334f9d7968acbd98b1d
Erick Peirson <erick.peirson@asu.edu>
2016-03-17 10:39:25 -0400

    added created_original to externalresource admin form

--- genecology/heroku_settings.py
+++ genecology/heroku_settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x^1$f$^=%!gdhmyx$%y8mn())y10dih)rcv8no0(ixyl%@upfl'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
