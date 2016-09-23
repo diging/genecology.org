f1819b2d71e233320ed2a96e5a26f9b89a608099
Erick Peirson <erick.peirson@asu.edu>
2016-09-23 13:11:35 -0400

    task config

--- blog/tasks.py
+++ blog/tasks.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import requests
 
 from blog.models import *
-from blog.evernote_api import sync_note
+from blog import evernote_api
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
@@ -12,4 +12,4 @@ from celery import shared_task
 @shared_task
 def sync_note(user_id, note_id):
     user = GenecologyUser.objects.get(pk=user_id)
-    sync_note(user, note_id)
+    evernote_api.sync_note(user, note_id)
