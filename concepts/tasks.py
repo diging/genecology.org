823a7a9ec0cff71d7b061827eab9c617ff0df0c7
Erick <erick.peirson@asu.edu>
2017-01-31 09:06:56 -0500

    wtf

--- concepts/tasks.py
+++ concepts/tasks.py
@@ -5,14 +5,14 @@ import requests
 from concepts.authorities import resolve, search
 
 
-@shared_task
+
 def resolve_concept(sender, instance):
     try:
         resolve(sender, instance)
     except requests.exceptions.ConnectionError:
         pass
 
-@shared_task
+
 def search_concept(query, pos='noun'):
     try:
         search(query, pos)
