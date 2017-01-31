2ade529db7e50ee4a763d1d3611dfab3113ef2d6
Erick <erick.peirson@asu.edu>
2017-01-31 09:02:35 -0500

    more celery nonsense

--- blog/tasks.py
+++ blog/tasks.py
@@ -7,9 +7,9 @@ from blog import evernote_api
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from celery import shared_task
 
-@shared_task
+
+
 def sync_note(user_id, note_id):
     user = GenecologyUser.objects.get(pk=user_id)
     evernote_api.sync_note(user, note_id)
